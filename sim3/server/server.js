816cdcaeb0835775a28a25ca76f60e7eeae5f7b1
Keith Didier <keithdidier@Keiths-MacBook-Air.local>
2017-09-08 14:51:41 -0600

    done

--- sim3/server/server.js
+++ sim3/server/server.js
@@ -1,45 +1,56 @@
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      massive = require('massive'),
-      cors = require('cors'),
-      chalk = require('chalk'),
-      session = require('express-session'),
-      passport = require('passport');
+require('dotenv').config();
 
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , massive = require('massive')
+    , cors = require('cors')
+    , session = require('express-session')
+    , passport = require('passport')
+    , chalk = require('chalk');
 
-const app = express();
-      
-app.use(session({
-    secret: process.env.SECRET, //**Required** This is the secret used to sign the session ID cookie. The secret is used to encrypt the session ID, session data is stored server side always, the ID is how its matched up. 
-    resave: false, //When true it forces the session to be saved back to the session store, even if the session was never modified during the request.
-    saveUninitialized: true //When true it forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified.
-    }));
-//Be sure to use express.session() before passport.session() to ensure that the login session is restored in the correct order.
-app.use(passport.initialize()); //This middleware is required to initialize Passport. 
-app.use(passport.session()); //If your application uses persistent login sessions, passport.session() middleware must also be used.
-      
+const strategy = './strategy';
+const friends_controller = require('./controllers/friends_controller');
 
+var app = express();
 app.use(bodyParser.json() );
 app.use(cors() );
 
-
-
+app.use( session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use( passport.initialize() );
+app.use( passport.session() );
+// passport.use( strategy );
 
 massive('localhost:3001/api/item'/*connectionString or URL*/).then(db => {
     app.set('bd', db)
-    /*ADD SEE FILE IF NEEDED 
-    app.get('db').init.seed().then(res => {
-        console.log(res)*/
+    /*ADD SEE FILE IF NEEDED */
+    app.get('db').init.seed().then(res => console.log(res))
 }).catch(err => {
-    console.log(err)
+    // console.log(err)
 });
 
+// This is invoked once to set things up
+passport.serializeUser( function( user, done ) {
+    done( null, user )
+} );
+// User comes from session - this is invoked for every endpoint
+passport.deserializeUser( function( user, done ) {
+    app.get('db').find_session_user( user[0].id ).then( user => {
+        return done( null, user[0] )
+    })
+} );
+
 //CONTROLLERS BELOW
+app.get('/api/friend/list', friends_controller.friends);
+app.post('/api/friend/add', friends_controller.addFriends);
+app.post('/api/friend/remove', friends_controller.deleteFriends);
 
 
-
-const portChalk = chalk.cyan.underline.bold;
 const port = 3001;
+const portChalk = chalk.cyan.underline.bold
 app.listen(port, () => {
-    console.log(portChalk(`Listening on port ${port}`));
+    console.log( portChalk(`Listening on port ${port}`) );
 })
